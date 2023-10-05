4c79b84f2e11c6c389a1751377af49a281f28d65
Tio Jobs <icarojobsoficial@gmail.com>
2023-10-05 10:32:29 -0300

    feat: finished cart and products routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,8 +12,8 @@ function App() {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path='/' element={Product} />
-                    <Route path='/cart' element={Cart} />
+                    <Route path='/' element={<Product />} />
+                    <Route path='/cart' element={<Cart />} />
                 </Routes>
             </Router>
         </div>
